2dcfdd9c00093d8dc9f7f5e4e50d00a81292d1cc
Pif302005 <checcofran717@gmail.com>
2025-05-13 10:27:07 +0200

    modifica stampe main

--- PSLD1/main.cpp
+++ PSLD1/main.cpp
@@ -62,7 +62,10 @@ int main() {
   while(scelta != 0)
   {
     std::cout << "" << std::endl;
-    std::cout << "Test da scegliere" << std::endl;
+    std::cout << "-------------------Benevenuto alla fine dei giochi-------------------" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Scegli il test con cui mettere alla prova il tuo codice: " << std::endl;
+    std::cout << "" << std::endl;
     std::cout << "1)Test Professore " << std::endl;
     std::cout << "2)Test numero 1 mio" << std::endl;
     std::cout << "3)Test numero 2 mio" << std::endl;
